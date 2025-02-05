b2cb1f82b7514c41246bffc4dd0ead8ce3557cdf
MehretuAb <mehertu.abreham@gmail.com>
2025-02-05 11:01:50 +0300

    Implement public route decorator and global auth guard
    - Added `@Public()` decorator to allow unauthenticated access to specific routes
    - Modified AuthGuard to skip authentication for public routes
    - Configured global AuthGuard in AuthModule

--- auth/constants.ts
+++ auth/constants.ts
@@ -1,5 +1,9 @@
+import { SetMetadata } from "@nestjs/common";
 
 export const jwtConstants = {
     secret: 'this is vvssdr18p15 secret key',
 };
+
+export const IS_PUBLIC_KEY = 'isPublic';
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
 
