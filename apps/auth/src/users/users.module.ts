775e80ff7df4407c431098c1f91d90ef4568de1d
MehretuAb <mehertu.abreham@gmail.com>
2025-02-13 11:18:04 +0300

    bug fix: remove imports

--- apps/auth/src/users/users.module.ts
+++ apps/auth/src/users/users.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { APP_GUARD } from '@nestjs/core';
-import { RolesGuard } from '@app/auth/guards/roles.guard';
 import { UserRepository } from './repositories/user.repository';
 import { DataSource } from 'typeorm';
 import { DatabaseModule } from 'libs/common/src/database/database.module';
 import { SmsModule } from 'libs/common/src/sms/sms.module';
 import { SignupSessionService } from '@app/auth/services/signup-session.service';
-import { LoggerModule } from 'libs/common/src/logger/logger.module';
 
 @Module({
   imports: [
